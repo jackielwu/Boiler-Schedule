1862e330a257365d19ecaf3fd6f064a85f6dbfc6
Sanjeet Suhag <suhagsanjeet@gmail.com>
2018-02-11 17:18:18 -0500

    Adds myPurdueSync script template.

---
+++ src/myPurdueSync.js
@@ -0,0 +1,19 @@
+// Obtain authentication token.
+function fetchAuth() {
+
+}
+
+// Validate authentication token.
+function authenticate() {
+
+}
+
+// Fetch courses.
+function fetchCourses() {
+
+}
+
+// Fetch student data.
+function fetchProfileData() {
+
+}
