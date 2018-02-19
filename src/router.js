9fdc2691fbf0b48c917980ca13f4a9bbb7dcffd4
Sanjeet Suhag <suhagsanjeet@gmail.com>
2018-02-18 22:09:47 -0500

    Adds Auth, basic sign in UI

--- src/router.js
+++ src/router.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('./auth');
 
 router.use(express.static(__dirname + '/public'));
 
+router.use(function(req, res, next) {
+    console.log(req.method, req.url);
+    next();
+});
+
 router.get('/', function(req, res) {
-  console.log(req);
   res.send('/public/index.html');
 });
 
-module.exports = router
+router.get('/purdue_signin',  auth.purdue.bounce, function (req, res) {
+  res.send('/public/index.html');
+});
+
+module.exports = router;
